c2db2a0fae8d78d6b518b94b970ff59e5dac838b
00332385299 <00332385299@JIPA-LABIN05-18.ifro.local>
2017-04-12 21:35:38 -0400

    Segundo commit

--- JavaApp/src/javaapp/JavaApp.java
+++ JavaApp/src/javaapp/JavaApp.java
@@ -15,6 +15,7 @@ public class JavaApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Teste");
         // TODO code application logic here
     }
     
